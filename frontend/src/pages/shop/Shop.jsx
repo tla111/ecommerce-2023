21b9c5a4f5ccf0b9e5e6290fc7ab51aba546d8fe
Tim La <tlamarketing111@gmail.com>
2023-01-01 05:54:51 -0500

    imported imports for shop

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
 
 const Shop = () => {
     return (
