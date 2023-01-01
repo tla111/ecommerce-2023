b3edc0d4338f33c4662059cc58d7f999dcd0edf3
Tim La <tlamarketing111@gmail.com>
2023-01-01 05:56:46 -0500

    able to map through products

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { PRODUCTS } from "../../products";
-import { Product } from "./product";
+import { PRODUCTS } from "../../ProductData";
+import Product from "./Product";
 import "./shop.css";
 
 const Shop = () => {
     return (
-        <div>Shop</div>
+        <div className="shop">
+            <div className="shopTitle">
+                <h1>PedroTech Shop</h1>
+            </div>
+
+            <div className="products">
+                {PRODUCTS.map((product) => (
+                    <Product data={product} />
+                ))}
+            </div>
+        </div>
     )
 }
 
