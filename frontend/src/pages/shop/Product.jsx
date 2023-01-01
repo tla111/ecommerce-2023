484b5123e77ed858a7da64709c591d996e8c4ff3
Tim La <tlamarketing111@gmail.com>
2023-01-01 05:58:11 -0500

    destrctured props data

--- frontend/src/pages/shop/Product.jsx
+++ frontend/src/pages/shop/Product.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
+
 const Product = () => {
+    const { id, productName, price, productImage } = props.data;
+
     return (
         <div>Product</div>
     )
