9223bf1ae90146183e97ddf9a003e75998f43da7
Tim La <tlamarketing111@gmail.com>
2023-01-02 06:22:57 -0500

    structured cartItem

--- frontend/src/pages/cart/CartItem.jsx
+++ frontend/src/pages/cart/CartItem.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
-const CartItem = () => {
+export const CartItem = (props) => {
+    const { id, productName, price, productImage } = props.data;
+    const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+        useContext(ShopContext);
+
     return (
-        <div>CartItem</div>
-    )
-}
-
-export default CartItem
+        <div className="cartItem">
+            <img src={productImage} />
+            <div className="description">
+                <p>
+                    <b>{productName}</b>
+                </p>
+                <p> Price: ${price}</p>
+                <div className="countHandler">
+                    <button onClick={() => removeFromCart(id)}> - </button>
+                    <input
+                        value={cartItems[id]}
+                        onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+                    />
+                    <button onClick={() => addToCart(id)}> + </button>
+                </div>
+            </div>
+        </div>
+    );
+};
