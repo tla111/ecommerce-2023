bf764bcc22e82dda83e3be6fb9fade402b13abe7
Tim La <tlamarketing111@gmail.com>
2023-01-02 06:21:06 -0500

    imported imports for cart

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/ShoppyContext";
+import { PRODUCTS } from "../../ProductData";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
     return (
