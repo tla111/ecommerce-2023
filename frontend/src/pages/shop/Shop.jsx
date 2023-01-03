db37fd4760b9538b90434c84ddc7d728b8bcfc3d
Tim La <tlamarketing111@gmail.com>
2023-01-03 04:59:45 -0500

    changed shop title

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -7,7 +7,7 @@ const Shop = () => {
     return (
         <div className="shop">
             <div className="shopTitle">
-                <h1>TimTech Shop</h1>
+                <h1>Tim's Tech Shop</h1>
             </div>
 
             <div className="products">
